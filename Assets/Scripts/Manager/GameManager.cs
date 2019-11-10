3f9140e374aac1606b59a83303c99a5aac23be48
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2019-11-10 11:22:10 -0800

    Set maxScore based on number of shapes in level

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,9 +7,7 @@ namespace Manager
     public class GameManager : Singleton<GameManager>
     {
         private static int _currentScore;
-        
-        // TODO: Base on current level by getting info from ShapeManager
-        private static int _maxScore = 2;
+        private static int _maxScore;
         
         private static readonly ScoreUpdatedEvent OnScoreUpdated = new ScoreUpdatedEvent();
         
@@ -30,7 +28,7 @@ namespace Manager
             OnScoreUpdated.AddListener(eventHandler);
         }
 
-        private static void SetMaxScore(int maxScore)
+        public static void SetMaxScore(int maxScore)
         {
             _maxScore = maxScore;
             OnScoreUpdated?.Invoke(_currentScore, _maxScore);
--- Assets/Scripts/Manager/ShapeManager.cs
+++ Assets/Scripts/Manager/ShapeManager.cs
@@ -11,6 +11,7 @@ namespace Manager
         private void Awake()
         {
             _shapes = FindObjectsOfType<ChangeShapeController>();
+            GameManager.SetMaxScore(_shapes.Length);
         }
 
         public void ApplyOnEachShape(Action<ChangeShapeController> action)
