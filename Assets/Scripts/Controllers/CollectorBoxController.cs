ab8ec2a564846102a94505dc46e287ee3414b0a0
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2019-11-10 10:37:45 -0800

    Fix bug where single shape is counted multiple times.

--- Assets/Scripts/Controllers/CollectorBoxController.cs
+++ Assets/Scripts/Controllers/CollectorBoxController.cs
@@ -1,4 +1,5 @@
-﻿using Manager;
+﻿using System;
+using Manager;
 using Misc;
 using UnityEngine;
 
@@ -7,12 +8,16 @@ namespace Controllers
     public class CollectorBoxController : MonoBehaviour
     {
         [SerializeField] private Sprite spriteTypeToCollect;
+        private bool _isColliding;
         
         private void OnTriggerEnter2D(Collider2D other)
         {
             // Ignore if it wasn't a shape-type
-            if (!other.CompareTag(Tag.Shape)) return;
-
+            if (!other.CompareTag(Tag.Shape) || _isColliding) return;
+            
+            // Shapes such as circles have multiple contact points. We need to check whether we are already colliding.
+            _isColliding = true;
+            
             Destroy(other.gameObject);
 
             // If it is the correct type of shape
@@ -24,6 +29,11 @@ namespace Controllers
             {
                 GameManager.DecrementScore();
             }
+        }
+
+        private void Update()
+        {
+            _isColliding = false;
         }
     }
 }
